40610b5df09e118caed785fa151f3c54bd9e022f
Wataru <wataru.kawagoe.dev@gmail.com>
2023-08-13 18:48:41 +0900

    Initial Commit

---
+++ ZeroBaseSlider/ZeroBaseSlider.h
@@ -0,0 +1,18 @@
+//
+//  ZeroBaseSlider.h
+//  ZeroBaseSlider
+//
+//  Created by Kawagoe Wataru on 2023/08/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZeroBaseSlider.
+FOUNDATION_EXPORT double ZeroBaseSliderVersionNumber;
+
+//! Project version string for ZeroBaseSlider.
+FOUNDATION_EXPORT const unsigned char ZeroBaseSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZeroBaseSlider/PublicHeader.h>
+
+
